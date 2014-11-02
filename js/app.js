e2bb1f51b763df7c3998b3ede5ee31ad65bb8b1f
sgwathen <samuelwathen@MAC.local>
2014-11-02 10:59:42 -0600

    added interactive

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-// JavaScript Document
+$(document).ready(function() {
+	$('.popular').mousedown(function(e) {
+        $(this).toggleClass('popularClick');
+    })
+	.mouseup(function(e) {
+        $(this).toggleClass('popularClick');
+    });
+	$('.go').mousedown(function(e) {
+        $(this).toggleClass('goClick');
+    })
+	.mouseup(function(e) {
+        $(this).toggleClass('goClick');
+    });
+	//submission of tag
+	$('.go').click(function(e) {
+		e.preventDefault();
+		var tag = $('#tagsearch').val();
+		//initiate ajax call
+		searchTags(tag);
+	});
+	$('.popular').click(function(e) {
+		e.preventDefault();
+		popularPics();
+	});
+});
