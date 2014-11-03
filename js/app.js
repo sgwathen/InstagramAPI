dfe7e19b28791cfe4083b2be346e00b8de791a10
sgwathen <samuelwathen@MAC.local>
2014-11-02 19:57:35 -0600

    fuck

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	//popularPics();
 	$('.popular').mousedown(function(e) {
         $(this).toggleClass('popularClick');
     })
@@ -22,4 +23,29 @@ $(document).ready(function() {
 		e.preventDefault();
 		popularPics();
 	});
+	$('#tagsearch').keypress(function(e) {
+		if(e.which == 13) {
+			e.preventDefault();
+			$('.go').click();	
+		}
+	});
 });
+//client-id: d5e832076cd44faa9fbcf7f98a2689bd
+var searchTags = function(tag) {
+	var getTags = $.ajax({
+		url: "https://api.instagram.com/v1/tags" + tag + "/media/recent?client_id=d5e832076cd44faa9fbcf7f98a2689bd",
+		dataType:"jsonp",
+		type:"GET"
+	}) .done(function(getTags){
+		var imageUrl = photo.images.standard_resolution.url;
+		var captionDiv = '<div class="caption"></div>'
+		$.each(getPopular.data, function(index, photo){
+			$('.outline').html('<a class="imglink" href="' + imageUrl + '"><img src="' + imageUrl + '"/>' + captionDiv);
+			if (photo.caption === null){
+				$('.caption').html('');	
+			} else {
+				$('.caption').html(photo.caption.text);
+			}
+		});
+	});		
+};
